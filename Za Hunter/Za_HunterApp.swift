1a082f8645133d6f846a1400f52da6715af5e884
Jack Kamaryt <jkamaryt46@gmail.com>
2023-02-09 09:10:05 -0600

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Jack Kamaryt on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
